f4c8f0de40216da1643cbc3e9541af980d113979
Ilya Zverev <zverik@textual.ru>
2011-06-24 15:53:28 +0000

    вынес учебник в отдельный файл, changelog тоже

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<? header("Content-Type: text/html; charset=utf-8"); ?>
+<?
+header("Content-Type: text/html; charset=utf-8");
+$versions = file_get_contents("version");
+$tested = preg_match('/tested[\w .:]+(\d{4,5})/', $versions, $matches) ? $matches[1] : 'X3';
+$latest = preg_match('/latest[\w .:]+(\d{4,5})/', $versions, $matches) ? $matches[1] : 'X3';
+?>
 <html>
 <head>
 <title>JOSM с самого начала</title>
@@ -29,22 +34,12 @@ body, td, p {
 <table cellspacing="0" cellpadding="0" border="0" width="100%">
 <tr>
 <td align="left" valign="top" rowspan="2" style="padding-right: 2em;">
-<p>Самый распространённый редактор — <a href="http://wiki.openstreetmap.org/wiki/RU:JOSM/Guide">JOSM</a>, рекомендую сразу его поставить и забыть о браузерных правках. Начать не так просто, как в НЯК или викимапии, но достаточно очевидно:
-<ol start="0">
-<li>Запускаете JOSM.
-<li>Нажимаете кнопку с зелёной стрелкой вниз и выбираете-скачиваете нужный участок карты (проверьте, чтобы галочки «данные» и «GPS-треки» были установлены).
-<li>В меню «Снимки» выбираете подложку, обычно Bing или IRS. <a href="http://wiki.openstreetmap.org/wiki/RU:Using_Aerial_Imagery">Привязываете её к трекам</a>.
-<li>«S» — выделить объект (точку, линию), «A» — добавить новый, справа есть панель тегов выбранного объекта.
-<li>Полезно прочитать некоторые <a href="http://wiki.openstreetmap.org/wiki/RU:WikiProject_Russia">правила рисования в России</a> и держать под рукой <a href="http://wiki.openstreetmap.org/wiki/RU:Map_Features">список элементов карты</a> (поиск по странице ftw).
-<li>Все вопросы можно задать в темах на форуме: <a href="http://forum.openstreetmap.org/viewtopic.php?id=10206">вопросы новичков</a> и <a href="http://forum.openstreetmap.org/viewtopic.php?id=2094">как обозначать</a>.
-<li>Поправив район, нажимаете зелёную стрелку вверх, кратко описываете суть правок и «загрузить».
-<li>Через 5-10 минут можно проверить на карте <a href="http://www.openstreetmap.org/">osm.org</a>, как оно нарисовалось.
-</ol>
+<? include("tutorial.html"); ?>
 </td>
 <td align="right" valign="top">
 <a href="http://josm.openstreetmap.de/wiki/Ru%3AWikiStart"><img src="josm.png" alt="JOSM" border="0"></a><br>
-<a href="http://josm.openstreetmap.de/josm-tested.jar">Загрузить стабильную версию 4064</a><br>
-<a href="http://josm.openstreetmap.de/josm-latest.jar">или версию в разработке 4157</a><br>
+<a href="http://josm.openstreetmap.de/josm-tested.jar">Загрузить стабильную версию <?=$tested ?></a><br>
+<a href="http://josm.openstreetmap.de/josm-latest.jar">или версию в разработке <?=$latest ?></a><br>
 <br>
 <a href="http://java.com/ru/download/index.jsp">Java</a><br>
 Установщики под <a href="http://josm.openstreetmap.de/download/windows/josm-setup.exe">Windows</a>, <a href="http://josm.openstreetmap.de/download/macosx/josm-macosx.zip">Mac OS X</a><br>
@@ -53,10 +48,20 @@ body, td, p {
 <tr><td align="left" valign="top" style="padding-top: 2em;">
 <p>Что изменилось:</p>
 <table cellspacing="0" cellpadding="0" border="0">
-<tr><td valign="top" style="padding-right: 8px;"><a href="http://josm.openstreetmap.de/changeset/4154/josm/">@4154</a><br><span class="changedate">21 июня</span></td>
-<td valign="top">поправлена загрузка отсутствующих членов для новых отношений <a href="http://josm.openstreetmap.de/ticket/6414" class="fixedbug">#6414</a></td></tr>
-<tr><td valign="top" style="padding-right: 8px;"><a href="http://josm.openstreetmap.de/changeset/4152/josm/">@4152</a><br><span class="changedate">19 июня</span></td>
-<td valign="top">исправлено нарушение целостности данных при соединении узлов <a href="http://josm.openstreetmap.de/ticket/6414" class="fixedbug">#6217</a></td></tr>
+<?
+$months = array('января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря', '13');
+foreach( file("changelog.txt") as $logline ) {
+	if( preg_match('/^@(\d+),\s*(\d+)\.(\d+):\s*(.+?)\s*$/', $logline, $matches) ) {
+		$id = $matches[1];
+		$date = $matches[2].' '.$months[$matches[3]-1];
+		$text = preg_replace('/#(\d+)/', '<a href="http://josm.openstreetmap.de/ticket/\1" class="fixedbug">\0</a>', $matches[4]);
+		?>
+<tr><td valign="top" style="padding-right: 8px;"><a href="http://josm.openstreetmap.de/changeset/<?=$id?>/josm/">@<?=$id?></a><br><span class="changedate"><?=$date?></span></td>
+<td valign="top"><?=$text?></td></tr>
+<?		
+	}
+}
+?>
 </table>
 </td></tr>
 </table>
