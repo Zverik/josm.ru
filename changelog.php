ae7a281fc690d1aba467a3f902bcbd89de7ec781
Ilya Zverev <zverik@textual.ru>
2011-06-26 20:25:28 +0000

    add changelog.php

---
+++ changelog.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Список изменений в JOSM</title>
+<style>
+body, td, p {
+	font-family: Arial, sans-serif;
+	font-size: 10pt;
+}
+.changedate {
+	font-size: 8pt;
+	color: gray;
+}
+.fixedbug {
+	text-decoration: line-through;
+}
+</style>
+</head>
+<body>
+<? include('changes.php'); ?>
+<p><a href="/">Вернуться на josm.ru</a></p>
+</body>
+</html>
